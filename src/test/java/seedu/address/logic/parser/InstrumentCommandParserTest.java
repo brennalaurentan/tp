633cf5911fa5f67b7e454600da7832cb70871744
Shaun Lai <shaun@littlebit.sg>
2024-04-10 00:45:25 +0800

    Fix style errors

--- src/test/java/seedu/address/logic/parser/InstrumentCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/InstrumentCommandParserTest.java
@@ -1,13 +1,15 @@
 package seedu.address.logic.parser;
 
-import org.junit.jupiter.api.Test;
-import seedu.address.logic.commands.InstrumentCommand;
-import seedu.address.model.person.Instrument;
-
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_SECOND_PERSONS;
+
+import org.junit.jupiter.api.Test;
+
+import seedu.address.logic.commands.InstrumentCommand;
+import seedu.address.model.person.Instrument;
+
 
 public class InstrumentCommandParserTest {
 
