614c745309c5ece6241ee48e45cacfd866123f97
Shaun Lai <shaun@littlebit.sg>
2024-04-10 00:38:57 +0800

    Add test cases for the InstrumentCommandParser

---
+++ src/test/java/seedu/address/logic/parser/InstrumentCommandParserTest.java
@@ -0,0 +1,34 @@
+package seedu.address.logic.parser;
+
+import org.junit.jupiter.api.Test;
+import seedu.address.logic.commands.InstrumentCommand;
+import seedu.address.model.person.Instrument;
+
+import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
+import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST_SECOND_PERSONS;
+
+public class InstrumentCommandParserTest {
+
+    private InstrumentCommandParser parser = new InstrumentCommandParser();
+
+    @Test
+    public void parse_validArgs_returnsInstrumentCommand() {
+        Instrument instrument = new Instrument("flute");
+        assertParseSuccess(parser, "1 2 i/flute", new InstrumentCommand(INDEX_FIRST_SECOND_PERSONS,
+                instrument));
+    }
+
+    @Test
+    public void parse_invalidIndexArg_throwsParseException() {
+        assertParseFailure(parser, "-11 i/flute", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                InstrumentCommand.MESSAGE_USAGE));
+    }
+
+    @Test
+    public void parse_invalidInstrumentArg_throwsParseException() {
+        assertParseFailure(parser, "1 i/!!!", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+                InstrumentCommand.MESSAGE_USAGE));
+    }
+}
