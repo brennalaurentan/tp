2ad1a1dbd1104c60fd0cd04b287df26dd5e0467e
brennalaurentan <122245137+brennalaurentan@users.noreply.github.com>
2024-04-13 04:20:05 +0800

    Fix checkstyle errors

--- src/test/java/seedu/address/commons/core/VersionTest.java
+++ src/test/java/seedu/address/commons/core/VersionTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static seedu.address.testutil.Assert.assertThrows;
 
 import org.junit.jupiter.api.Test;
-import seedu.address.logic.parser.Parser;
 
 public class VersionTest {
 
